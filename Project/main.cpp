c66905b0ade624946005cac994dc36a77dabd9e2
CtrlShift-Esc <jehovah0427@gmail.com>
2022-09-26 13:19:59 +0900

    update

---
+++ Project/main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int left, int right) {
+	int answer = 0;
+	for (int i = left; i <= right; i++)
+	{
+		int cnt = 0;
+		for (int j = 1; j <= i; j++)
+		{
+			if (i%j == 0)
+				cnt++;
+		}
+		answer = cnt % 2 == 0 ? answer + i : answer - i;
+	}
+	return answer;
+}
+
+void main()
+{
+	//test
+	//auto ret = solution(24, 27);
+}
